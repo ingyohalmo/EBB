f8e3be80e976f67ca909d3a58f1545df5f031c50
yohalmito4 <yohalmito4@gmail.com>
2016-03-14 12:25:19 -0600

    se agrega documentacion necesaria

--- DR12004_2016_03_15/DR12004_2016_03_15.ino
+++ DR12004_2016_03_15/DR12004_2016_03_15.ino
@@ -1,8 +1,25 @@
-/*********************************************************************
-  Yohalmo Alexander Diaz Ramirez
-  DR12004
-  EBB-115 Ciclo 1 2016
-*********************************************************************/
+/********************************************************************************************************************************
+  (2016/03/14)
+  Universidad Nacional de El Salvador, Facultad de Ingenieria Y Arquitectura.
+  EBB-115 Ciclo 1 2016. 
+  
+  Ing. Wilber Calderon.
+
+  Yohalmo Alexander Diaz Ramirez.
+  DR12004.
+
+  El siguiente codigo ha sido creado con el fin aprender la manera corecta 
+  para utiliza la pantalla LCD Nokia-5110, utilizando 2 potenciometros se trazan
+  rectas y diagonales de manera automatica y se establece un porcentaje para su movimiento
+  de 0-45 se mueve hacia la derecha y arriba
+  de 45-55 no hay ningun movimiento
+  de 55-100 se mueve a la  izquierda y abajo
+
+  REFERENCIAS:
+  https://learn.sparkfun.com/tutorials/graphic-lcd-hookup-guide
+  https://masamuneshin.wordpress.com/2011/04/30/proyecto-arduino-i-lectura-limpia-de-potenciometro-para-control-de-servomotor/
+  https://learn.adafruit.com/downloads/pdf/nokia-5110-3310-monochrome-lcd.pdf
+********************************************************************************************************************************/
 
 #include <SPI.h>
 #include <Adafruit_GFX.h>
@@ -24,25 +41,25 @@ Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 4, 3);
 #define LOGO16_GLCD_HEIGHT 16
 #define LOGO16_GLCD_WIDTH  16
 
-int SensorPin[] = {A0, A1};
-int Vector[] = {0, 0};
-int Tamano[] = {83, 47};
+int SensorPin[] = {A0, A1};//se guardan los pines de los potenciometros
+int Vector[] = {0, 0};//vector inicializado para guardar coordenadas X,Y
+int Tamano[] = {83, 47};//vector que guarda los tamaños maximos de X,Y
 
 
 void setup()   {
   Serial.begin(9600);
 
-  display.begin();// inicializar
+  display.begin();//inicializar
   
-  display.setContrast(60);// arreglar el contraste
+  display.setContrast(60);//arreglar el contraste
 
-  display.clearDisplay();   // limpiar patalla y buffer
+  display.clearDisplay();//limpiar patalla y buffer
 
 }
 
 
 void loop() {
-  //  display.clearDisplay();   // limpiar patalla y buffer
+  //  display.clearDisplay();   //limpiar patalla y buffer
 
   //recorre el vector SensorPin para ver las mediciones hechas y a luego sumarlas a Vector
   for (int i = 0; i < 2; i++) {
