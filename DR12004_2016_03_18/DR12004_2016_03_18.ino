8b99dc54e47d2ec020c44ff394234d85bddfa1e6
yohalmito4 <yohalmito4@gmail.com>
2016-03-30 10:11:06 -0600

    falta imprimir frecuencia y voltaje maximo

--- DR12004_2016_03_18/DR12004_2016_03_18.ino
+++ DR12004_2016_03_18/DR12004_2016_03_18.ino
@@ -1,5 +1,5 @@
 /********************************************************************************************************************************
-  (2016/03/14)
+  (2016/03/31)
   Universidad Nacional de El Salvador, Facultad de Ingenieria Y Arquitectura.
   EBB-115 Ciclo 1 2016. 
   
@@ -7,18 +7,17 @@
 
   Yohalmo Alexander Diaz Ramirez    DR12004.
 
-  OBJETIVO: crear un osciloscopio con LCD Nokia-5110
+  OBJETIVO: crear un osciloscopio con LCD Nokia-5110 (84x48)
 
   CIRCUITO:
-  -Potenciometro X(Analogico 0)
-  -Potenciometro Y(Analogico 1)
-  -Ambos a Tierra y 5V
+  -Potenciometro (Analogico 0)
+  -A Tierra y 5V
   
   LCD De Izquierda a derecha(al 4050):
   -RST al PIN 2 del 4050
   -CE al PIN 4 del 4050
   -DC al PIN 6 del 4050
-  -DIN al PIN 14 del 4050
+  -DIN al PIN 15 del 4050
   -CLK al PIN 12 del 4050
   -VCC a 3.3V
   -LIGHT a Tierra
@@ -38,28 +37,13 @@
   http://www.thingiverse.com/thing:184218
   http://blog.novaeletronica.com.br/en/osciloscopio-usando-arduino/
 ********************************************************************************************************************************/
-#define FASTADC 1
-
-// defines for setting and clearing register bits
-#ifndef cbi
-#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
-#endif
-#ifndef sbi
-#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
-#endif
 
 #include "Adafruit_GFX.h"
 #include "Adafruit_PCD8544.h"
 #define   LARGO   250
 
-// pin 2 - Serial clock out (SCLK)
-// pin 3 - Serial data out (DIN)
-// pin 5 - Data/Command select (D/C)
-// pin 7 - LCD chip select (CS)
-// pin 6 - LCD reset (RST)
 Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 4, 3);
 
-//Analog Pins
 int channelAI = A0;      // entrada
 int delayAI = A1;       // potenciometro delay
 
@@ -71,18 +55,11 @@ int lecturas[ LARGO ];
 int counter = 0;
 
 void setup() {
-
-  #if FASTADC
-  // asigna preescala en 4 (010)
-  cbi(ADCSRA,ADPS2) ;
-  sbi(ADCSRA,ADPS1) ;
-  cbi(ADCSRA,ADPS0) ;
-  #endif
     
   Serial.begin(9600);
   
   display.begin();
-  display.setContrast(60); 
+  display.setContrast(50); 
   display.clearDisplay();     // limpiar pantalla y buffer
 
   // Texto de Introduccion
@@ -96,7 +73,7 @@ void setup() {
   display.println("Yohalmo");
   display.println("      EBB-115");
   display.display();
-  delay(500);
+  delay(1000);
   display.clearDisplay();
  
 }
@@ -106,8 +83,8 @@ void loop()
 {  
   
   delayVariable = analogRead(delayAI);
-  delayVariable = delayVariable/10;
-  scale = 47.0/1023.0;   
+  delayVariable = delayVariable/100;
+  scale = 47.0/1023.0;  
   
   //guarda lecturas
   for(xContar = 0; xContar < LARGO ; xContar += 1){                                 
@@ -117,8 +94,8 @@ void loop()
     }
   
   display.clearDisplay();
-
-  DibujarEjes();
+  
+  DibujarEjes(); // Funcion para dibujar los ejes
 
   for(xContar = 0; xContar < 85; xContar += 1){
        display.drawPixel(xContar, 47-lecturas[xContar], BLACK);
--- DR12004_2016_03_18/DibujarEjes.ino
+++ DR12004_2016_03_18/DibujarEjes.ino
@@ -1,9 +1,9 @@
 void DibujarEjes(){
 //Dibuja lineas de referencia del voltaje
-  display.drawLine( 10, 0, 10, 47, BLACK);
-  display.drawLine( 5, 47-(.166 *1023.0 * scale), 10, 47-(.166 *1023.0 * scale), BLACK);
-  display.drawLine( 0, 47-(.33 *1023.0 * scale), 10, 47-(.33 *1023.0 * scale), BLACK);
-  display.drawLine( 5, 47-(.5 *1023.0 * scale), 10, 47-(.5 *1023.0 * scale), BLACK);
-  display.drawLine( 0, 47-(.66 *1023.0 * scale), 10, 47-(.66 *1023.0 * scale), BLACK);
-  display.drawLine( 5, 47-(.84 *1023.0 * scale), 10, 47-(.84 *1023.0 * scale), BLACK);
+  display.drawLine( 42, 0, 42, 48, BLACK);
+  display.drawLine( 37, 47-(.166 *1023.0 * scale), 47, 47-(.166 *1023.0 * scale), BLACK);
+  display.drawLine( 37, 47-(.33 *1023.0 * scale), 47, 47-(.33 *1023.0 * scale), BLACK);
+  display.drawLine( 37, 47-(.5 *1023.0 * scale), 47, 47-(.5 *1023.0 * scale), BLACK);
+  display.drawLine( 37, 47-(.66 *1023.0 * scale), 47, 47-(.66 *1023.0 * scale), BLACK);
+  display.drawLine( 37, 47-(.84 *1023.0 * scale), 47, 47-(.84 *1023.0 * scale), BLACK);
 }
